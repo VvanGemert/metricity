e45538c7b41cea1efead35c193dc237c628b5560
VvanGemert <vincent@floorplanner.com>
2014-05-11 21:19:53 +0200

    Added first entry to send UDP requests.

--- lib/metricity.rb
+++ lib/metricity.rb
@@ -1,9 +1,13 @@
+require 'json'
 require 'metricity/version'
 require 'metricity/plugin'
 
 # Metricity
 module Metricity
   def self.init
-    Plugin.init
+    u1 = UDPSocket.new
+    msg = { test: 'woof' }
+    u1.send msg.to_json, 0, "127.0.0.1", 9888
+    # Plugin.init
   end
 end
