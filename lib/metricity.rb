37e30b04d89ed7dd583e22cb45e781fe65ddc323
VvanGemert <vincent@floorplanner.com>
2014-08-06 09:32:24 +0200

    Example push

--- lib/metricity.rb
+++ lib/metricity.rb
@@ -6,7 +6,10 @@ require 'metricity/pigeon'
 module Metricity
   def self.init
     pigeon = Metricity::Pigeon.new
-    pigeon.package({ test: 'woof' })
+    pigeon.package(
+      'time' => Time.now.utc.to_s,
+      'type' => 'memory_usage',
+      'objects' => { 'rails' => 50, 'delayed_job' => 100 })
     pigeon.fly!
   end
 end
