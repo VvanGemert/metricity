7a1721b5c9f0f48f901277362c50522be1c7c077
VvanGemert <vincent@floorplanner.com>
2014-11-11 16:01:44 +0100

    Adding linux specific code for memory metric

--- lib/metricity/metrics/memory.rb
+++ lib/metricity/metrics/memory.rb
@@ -2,10 +2,24 @@
 module Metricity
   # Plugin
   module Metric
-    # System
+    # Memory
     module Memory
       def self.run(platform)
-        mac_specific
+        if platform[:os] == :mac
+          mac_specific
+        elsif platform[:os] == :linux
+          linux_specific
+        end
+      end
+
+      def self.linux_specific
+        raw = `echo | top -bn1`
+        raw = raw.split("\n")
+        mem_usage = raw[3].split(' ')
+        total = mem_usage[2]
+        used = mem_usage[4]
+        free = mem_usage[6]
+        { total: to_mb(total), used: to_mb(used), free: to_mb(free) }
       end
 
       def self.mac_specific
@@ -16,6 +30,10 @@ module Metricity
         used = mem_usage[3].chop!
         free = total.to_i - used.to_i
         { total: total.to_i, used: used.to_i, free: free }
+      end
+      
+      def self.to_mb(val)
+        (val.to_f / 1000).to_i
       end
     end
   end
