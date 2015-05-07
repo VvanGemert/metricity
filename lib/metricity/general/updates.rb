a5db93620343fd587b9d19e3b5f54a3b19dce968
VvanGemert <vincent@floorplanner>
2015-05-07 22:28:35 +0200

    Fixed output for updates report

--- lib/metricity/general/updates.rb
+++ lib/metricity/general/updates.rb
@@ -26,7 +26,7 @@ module Metricity
 
     def self.find_apt_updates
       raw = `{ apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "$1 (\e[1;34m$2\e[0m -> \e[1;32m$3\e[0m)\n"}';} | while read -r line; do echo -en "$line\n"; done;`
-      output = raw.split("\n")
+      raw.split("\n")
     end
   end
 end
